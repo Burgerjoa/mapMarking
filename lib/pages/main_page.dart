4079b4ac55132c77b105013edd40fcf5f58c2e96
Burgerjoa <aikyan@kakao.com>
2023-09-07 00:32:50 +0900

    누더기골렘

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MainPage());
@@ -66,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
           });
         },
         children: [
-          HomeScreen(),
+          map_widget(),
           PublicTransportScreen(),
           NavigationScreen(),
           NearbyScreen(),
@@ -115,27 +116,27 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
         selectedItemColor: Colors.greenAccent,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-              context: context,
-              builder: (context) {
-                return AlertDialog(
-                  title: Text("내 위치"),
-                  content: Text("현재 위치"),
-                  actions: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text("닫기"),
-                    )
-                  ],
-                );
-              });
-        },
-        child: Icon(Icons.my_location),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     showDialog(
+      //         context: context,
+      //         builder: (context) {
+      //           return AlertDialog(
+      //             title: Text("내 위치"),
+      //             content: Text("현재 위치"),
+      //             actions: [
+      //               TextButton(
+      //                 onPressed: () {
+      //                   Navigator.pop(context);
+      //                 },
+      //                 child: Text("닫기"),
+      //               )
+      //             ],
+      //           );
+      //         });
+      //   },
+      //   child: Icon(Icons.my_location),
+      // ),
     );
   }
 }
@@ -181,29 +182,172 @@ class CustomSearchDelegate extends SearchDelegate {
 }
 
 // 홈 화면
-class HomeScreen extends StatelessWidget {
+class map_widget extends StatefulWidget {
+  const map_widget({Key? key, required}) : super(key: key);
+
+  @override
+  State<map_widget> createState() => _map_widgetState();
+}
+
+class _map_widgetState extends State<map_widget> {
+  late GoogleMapController _controller;
+  bool _myLocationEnabled = false;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    final position = await Geolocator.getCurrentPosition();
+    final cameraPosition = CameraPosition(
+      target: LatLng(position.latitude, position.longitude),
+      zoom: 18,
+    );
+    _controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+    setState(() {
+      _myLocationEnabled = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Map<String, dynamic>> places = [
+      {
+        'name': 'Place 1',
+        'latitude': 37.532600,
+        'longitude': 127.024612,
+      },
+      {
+        'name': 'Place 2',
+        'latitude': 37.532700,
+        'longitude': 127.024712,
+      },
+      {
+        'name': 'Place 3',
+        'latitude': 37.532800,
+        'longitude': 127.024812,
+      },
+      {
+        'name': 'Place 4',
+        'latitude': 37.532900,
+        'longitude': 127.024912,
+      },
+      {
+        'name': 'Place 5',
+        'latitude': 37.533000,
+        'longitude': 127.025012,
+      },
+      {
+        'name': 'Place 6',
+        'latitude': 37.533100,
+        'longitude': 127.025112,
+      },
+    ];
+    return CupertinoPageScaffold(
+      child: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: LatLng(37.532600, 127.024612), // 서울 시청 위치
+              zoom: 18,
+            ),
+            myLocationEnabled: _myLocationEnabled,
+            myLocationButtonEnabled: false,
+            markers: Set<Marker>.of(places.map((place) {
+              return Marker(
+                markerId: MarkerId(place['name']),
+                position: LatLng(place['latitude'], place['longitude']),
+                infoWindow: InfoWindow(title: place['name']),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                          builder: (context) => MarkerDetailScreen()));
+                },
+              );
+            })),
+            onMapCreated: (controller) => _controller = controller,
+          ),
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: FloatingActionButton(
+              onPressed: _getCurrentLocation,
+              foregroundColor: Colors.black,
+              backgroundColor: Colors.white,
+              elevation: 8, // 그림자 크기
+              child: Icon(Icons.my_location),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10), // 버튼 모서리 둥글기
+              ),
+            ),
+          ),
+          Positioned(
+            top: 54,
+            child: CupertinoButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    CupertinoPageRoute(
+                        builder: (context) => MarkerDetailScreen()));
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width - 21,
+                height: 44,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: Text(
+                        '위치 검색',
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 13.0),
+                    ),
+                  ],
+                ),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(8))),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MarkerDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("홈 화면"),
+        title: Text('Marker Detail'),
       ),
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(37.7749, -122.4194), // 초기 위치 설정
-          zoom: 12, // 초기 줌 레벨 설정
-        ),
-        markers: Set.from([
-          Marker(
-            markerId: MarkerId('marker_id'), // 마커 식별자
-            position: LatLng(37.7749, -122.4194), // 마커 위치 설정
-            infoWindow: InfoWindow(title: 'San Francisco'), // 정보 창 설정
-          ),
-        ]),
-        zoomControlsEnabled: false, // 줌 컨트롤 활성화 여부
-        // 나머지 GoogleMap 속성들...
+      body: Center(
+        child: Text('Marker detail screen'),
       ),
     );
+  }
+}
+
+class marker_widget extends StatelessWidget {
+  const marker_widget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
 
